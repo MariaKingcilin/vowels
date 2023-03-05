17a37dd537405409215286046fe1bb71aca403e0
mariakingcilin <mariakingcilin@gmail.com>
2023-03-05 12:07:06 +0530

    add

---
+++ main.js
@@ -0,0 +1,84 @@
+var x=prompt("Enter your name"),count=0;
+for(i=0;i<x.length;i++)
+	{
+	if(x[i]=="a"||x[i]=="e"||x[i]=="i"||x[i]=="o"||x[i]=="u")
+	{
+	count++;
+	}
+	}
+document.write(count);
+
+var x=[1,2,3,4,5,6,7,8];
+for(i=0;i<x.length;i++){
+	if(x[i]%2==0){
+		document.write(x[i]);
+		document.write("<br>");
+	}
+}
+for(i=1;i<=6;i++)
+	{
+	for(j=1;j<=i;j++)
+		document.write(i);
+		document.write("<br>");
+	}
+for(i=5;i>=1;i--)
+	{
+	for(j=1;j<=i;j++)
+	{
+	document.write(j);
+	}
+	document.write("<br>");
+	}
+var a=5,b="5";
+document.write(5==="5");
+document.write(a==="b");	
+for(i=5;i>=1;i--)
+	{
+	for(j=1;j<=i;j++)
+	{
+	document.write(i);
+	}
+	document.write("<br>");
+	}
+for(i=1;i<=5;i++)
+	{
+	for(j=5;j>=i;j--)
+	{
+	document.write(i);
+	}
+	document.write("<br>");
+	}
+var x=[1,2,3,4,5];
+for(i=0;i<=x.length;i++)
+	{
+	for(j=5;j>=x[i];j--)
+	{
+		document.write(x[i]);
+	}
+	document.write("<br>");
+	}
+var x=prompt("Enter your name"),count=0;
+for(i=0;i<x.length;i++)
+	{
+	if(x[i]=="a"||x[i]=="e"||x[i]=="i"||x[i]=="o"||x[i]=="u")
+	{
+	count++;
+	}
+	}
+	var x=5;
+function add(x)
+{
+	let a=5,b=7;
+	c=a+b;
+	console.log(x);
+	return a;
+}
+console.log(add())
+add(x)
+let name="kingcilin";
+document.write(name.length)
+
+
+
+
+
